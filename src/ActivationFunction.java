70c5e04dc9828c71d32b7be5dfed1a405003e72f
mdi9824 <marin2canini13@gmail.com>
2025-02-17 17:23:48 +0100

    Changement de ActivationFunction et InitializationFunction en enumerations pour plus de lisibilité

--- src/ActivationFunction.java
+++ src/ActivationFunction.java
@@ -1,97 +1,24 @@
-import java.util.Random;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public abstract class ActivationFunction implements Function<Double,Double> {
+public enum ActivationFunction {
+    ReLU(InitializationFunction.He, a -> Math.max(0, a)),
+    TanH(InitializationFunction.Xavier, Math::tanh),
+    Sigmoid(InitializationFunction.LeCun, a -> 1 / (1 + Math.exp(-a))),
+    SoftMax(InitializationFunction.LeCun, a -> 1 / (1 + Math.exp(-a)));  // SoftMax avec la même fonction pour l'instant.
 
-    /**
-     * En function de la fonction d'activation choisie, on va initializer les
-     * neurones différemment. Cela permet d'éviter des problèmes comme le
-     * vanishing gradient problem pour les réseaux plus compliqués.
-     */
-    public InitializationFunction initializationFunction;
+    public final InitializationFunction initializationFunction;
+    public final Function<Double, Double> function;
 
-
-    /**
-     * static Activation functions.
-     */
-    public static final ReLU ReLU = new ReLU();
-    public static final TanH TanH = new TanH();
-    public static final Sigmoid Sigmoid = new Sigmoid();
-
-    /**
-     * Initialization functions.
-     */
-
-    public static final InitializationFunction test = null;
-
-    public static final class ReLU extends ActivationFunction {
-
-        /**
-         * On tire une paire poids/biais aléatoire selon l'initialization de He
-         * Voir <a href="https://arxiv.org/abs/1502.01852">...</a>
-         */
-        public ReLU() {
-            this.initializationFunction = InitializationFunction.He;
-        }
-
-        @Override
-        public Double apply(Double aDouble) {
-            return Math.max(0,aDouble);
-        }
+    ActivationFunction(InitializationFunction initializationFunction, Function<Double, Double> function) {
+        this.initializationFunction = initializationFunction;
+        this.function = function;
     }
 
-    public static final class TanH extends ActivationFunction {
-
-        /**
-         * On tire une paire poids/biais aléatoire selon l'initialization Normale de Xavier Glorot
-         * Voir TODO find arxiv link
-         */
-        public TanH() {
-            this.initializationFunction = InitializationFunction.Xavier;
-        }
-
-        @Override
-        public Double apply(Double aDouble) {
-            return Math.tanh(aDouble);
-        }
+    public double apply(double value) {
+        return function.apply(value);
     }
 
-    public static final class Sigmoid extends ActivationFunction {
-
-        /**
-         * On tire une paire poids/biais aléatoire selon l'initialization UNIFORME de Xavier Glorot
-         * Voir TODO find arxiv link
-         */
-        public Sigmoid() {
-            this.initializationFunction = InitializationFunction.LeCun;
-        }
-
-        @Override
-        public Double apply(Double aDouble) {
-            return 1 / (1 + Math.exp(-aDouble));
-
-        }
+    public InitializationFunction getInitializationFunction() {
+        return initializationFunction;
     }
-
-    public static final class SoftMax extends ActivationFunction {
-
-        /**
-         * On tire une paire poids/biais aléatoire selon l'initialization UNIFORME de Xavier Glorot
-         * Voir TODO find arxiv link
-         */
-        public SoftMax() {
-            this.initializationFunction = InitializationFunction.LeCun;
-        }
-
-        @Override
-        public Double apply(Double aDouble) {
-            return 1 / (1 + Math.exp(-aDouble));
-
-        }
-    }
-
 }
-
-
-
--- src/InitializationFunction.java
+++ src/InitializationFunction.java
@@ -1,43 +1,30 @@
 import java.util.Random;
 import java.util.function.BiFunction;
 
-public class InitializationFunction {
+public enum InitializationFunction {
 
-    public BiFunction<Integer,Integer,Double> getRandomWeight;
-    public BiFunction<Integer,Integer,Double> getRandomBias;
+    He(
+            (inputSize, layerSize) -> new Random().nextGaussian() * Math.sqrt(2.0 / inputSize),
+            (inputSize,layerSize) -> 0.01
+    ),
 
-    // Static access
-    public static final InitializationFunction He = new He();
-    public static final InitializationFunction Xavier = new Xavier();
-    public static final InitializationFunction LeCun = new LeCun();
+    Xavier(
+            (inputSize, layerSize) -> new Random().nextGaussian() * Math.sqrt(1.0 / (inputSize+layerSize)),
+            (inputSize, layerSize) -> 0.01
+    ),
 
-    public static final class He extends InitializationFunction {
-        /**
-         * On tire une paire poids/biais aléatoire selon l'initialization de He
-         * Voir <a href="https://arxiv.org/abs/1502.01852">...</a>
-         */
-        public He() {
-            this.getRandomWeight = (inputSize, layerSize) -> new Random().nextGaussian() * Math.sqrt(2.0 / inputSize);
-            this.getRandomBias = (inputSize,layerSize) -> 0.01;
-        }
-    }
+    LeCun(
+            (inputSize, layerSize) -> new Random().nextGaussian() * Math.sqrt(1.0 / inputSize),
+            (inputSize, layerSize) -> 0.01
+    );
 
-    public static final class Xavier extends InitializationFunction {
-        /**
-         * On tire une paire poids/biais aléatoire selon l'initialization de Xavier
-         * Voir // TODO find xavier glorot initialization paper link on arxiv
-         */
-        public Xavier() {
-            this.getRandomWeight = (inputSize,layerSize) -> new Random().nextGaussian() * Math.sqrt(1.0 / (inputSize+layerSize));
-            this.getRandomBias = (inputSize,layerSize) -> 0.01;
-        }
-    }
 
-    public static final class LeCun extends InitializationFunction {
-        public LeCun() {
-            this.getRandomWeight = (inputSize, layerSize) -> new Random().nextGaussian() * Math.sqrt(1.0 / inputSize);
-            this.getRandomBias = (inputSize,layerSize) -> 0.01;
-        }
+    public final BiFunction<Integer,Integer,Double> getRandomWeight;
+    public final BiFunction<Integer,Integer, Double> getRandomBias;
+
+    InitializationFunction(BiFunction<Integer,Integer,Double> getRandmWeight, BiFunction<Integer,Integer,Double> getRandomBias){
+        this.getRandomWeight = getRandmWeight;
+        this.getRandomBias = getRandomBias;
     }
 
 }
